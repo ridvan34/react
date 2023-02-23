a99d2126f36dc2daf56a803aae400be851c65079
Ridvan Egricayir <1-root@users.noreply.192.168.178.70>
2023-02-23 19:34:47 +0100

    Kategoriler array icine eklendi ve ilk sayfada gösterildi

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+const App = () => {
+    const imgAdresse = "https://st1.myideasoft.com/shop/zr/76/myassets/products/942/1115261-3802.png?revision=1666435310";
 
-function App() {
-  return (
-    <div>
-        Hello World in develop branch
-    </div>
-  );
+    const categories = [
+        {
+            id: 1,
+            title: 'Ceket'
+        },
+        {
+            id: 2,
+            title: 'Gömlek'
+        },
+        {
+            id: 3,
+            title: 'Ayakkabi'
+        },
+        {
+            id: 4,
+            title: 'Pantolon'
+        },
+        {
+            id: 5,
+            title: 'Kazak'
+        }
+
+    ]
+
+    return (
+        <div className='categories-container'>
+            { categories.map((category) => (
+                <div className='category-container' id={`category_${category.id}`}>
+                    <div className='background-image' />
+                    <img src={ imgAdresse } height='300px' />
+                    <div className='category-body-container'>
+                        <h2>{ category.title }</h2>
+                        <p>Di haydi</p>
+                    </div>
+                </div>
+            )) }
+        </div>
+    );
 }
 
 export default App;
